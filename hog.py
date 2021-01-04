e91ea8bf687813d80836e775e6e12467f8307ecd
Heramb Lonkar <heramblonkar@gmail.com>
2021-01-04 15:48:25 +0530

    Completed Problem 6

--- hog.py
+++ hog.py
@@ -182,9 +182,11 @@ def play(strategy0, strategy1, score0=0, score1=0, dice=six_sided,
             if extra_turn(score0, score1) and score0 < goal:
                 pass
             elif score0 >= goal:
+                say = say(score0, score1)
                 break
             else:     
                 who = other(who)
+            say = say(score0, score1)
         while who == 1:
             player1_num_rolls = strategy1(score1, score0)
             score1 = score1 + take_turn(player1_num_rolls, score0, dice)
@@ -193,12 +195,14 @@ def play(strategy0, strategy1, score0=0, score1=0, dice=six_sided,
             elif score1 >= goal:
                 break
             else:
-                who = other(who) 
+                who = other(who)
+            say = say(score0, score1)
 
     # END PROBLEM 5
     # (note that the indentation for the problem 6 prompt (***YOUR CODE HERE***) might be misleading)
     # BEGIN PROBLEM 6
     "*** YOUR CODE HERE ***"
+
     # END PROBLEM 6
     return score0, score1
 
